d200ef2345623707ad2a42c75924a243c0473692
Michael Evans (iMac) <michael.evans.us@gmail.com>
2021-01-19 22:41:30 -0600

    First commit, but almost done with the core network stuff.

--- RESTfulAppCoda/ViewController.swift
+++ RESTfulAppCoda/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let rest = RestManager()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getUsersList()
+    }
+    
+    func getUsersList() {
+        guard let url = URL(string: "https://reqres.in/api/users") else { return }
+        
+        rest.makeRequest(toURL: url, withHttpMethod: .get) { (results) in
+            if let data = results.data {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                guard let userData = try? decoder.decode(UserData.self, from: data) else { return }
+                print(userData.description)
+            }
+            
+            print("\n\nResponse HTTP Headers:\n")
+            
+            if let response = results.response {
+                for (key, value) in response.headers.allValues() {
+                    print(key, value)
+                }
+            }
+        }
     }
 
 
